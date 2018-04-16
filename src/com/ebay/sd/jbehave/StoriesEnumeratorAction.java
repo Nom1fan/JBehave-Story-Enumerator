439d2ac25613cc8a77aa8381bb7921295e29a6bb
mmerhav <Xla@123$$>
2018-04-16 11:26:23 +0300

    Update enumerator action

--- src/com/ebay/sd/jbehave/StoriesEnumeratorAction.java
+++ src/com/ebay/sd/jbehave/StoriesEnumeratorAction.java
@@ -4,25 +4,32 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Created by mmerhav on 20/3/2017.
  */
 public class StoriesEnumeratorAction extends AnAction {
 
-    @Override
-    public void actionPerformed(AnActionEvent e) {
-        System.out.println(e);
-        String startFrom = Messages.showInputDialog("Give enumerator start value", "Title", null, "1", null);
-        System.out.println(startFrom);
-        StoriesEnumerator storiesEnumerator = new StoriesEnumeratorImpl();
-        try {
-            storiesEnumerator.enumerateStory(((EditorImpl)e.getDataContext().getData("editor")).getVirtualFile().getCanonicalPath(), Integer.valueOf(startFrom));
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        // TODO: insert action logic here
-    }
+	@Override
+	public void actionPerformed(AnActionEvent e) {
+		System.out.println(e);
+		String startFrom = Messages.showInputDialog("Give enumerator start value", "Title", null, "1", null);
+		EditorImpl editor = (EditorImpl) e.getDataContext().getData("editor");
+
+		Objects.requireNonNull(editor, startFrom);
+
+		VirtualFile virtualFile = editor.getVirtualFile();
+
+		System.out.println(startFrom);
+		StoriesEnumerator storiesEnumerator = new StoriesEnumeratorImpl();
+		try {
+			storiesEnumerator.enumerateStory(virtualFile.getCanonicalPath(), Integer.valueOf(startFrom));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
 }
